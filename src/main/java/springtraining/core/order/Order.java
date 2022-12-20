413cbff3d24253167b517816fe01ee5eed8fffc9
majayong71 <com4032647@gmail.com>
2022-12-20 18:08:01 +0900

    springboot principle training ver_a.3

---
+++ src/main/java/springtraining/core/order/Order.java
@@ -0,0 +1,16 @@
+package springtraining.core.order;
+
+public class Order {
+
+    private Long memberId;
+    private String itemName;
+    private int itemPrice;
+    private int discountPrice;
+
+    public Order(Long memberId, String itemName, int itemPrice, int discountPrice) {
+        this.memberId = memberId;
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.discountPrice = discountPrice;
+    }
+}
